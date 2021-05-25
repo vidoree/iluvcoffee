6cb6cb25b1ae3ffc438bd22482a33ead0d1e544e
Dora Sivo <sivodora@gmail.com>
2021-05-25 16:34:02 +0200

    feat(coffees): add basic REST API application

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    new ValidationPipe({
+      forbidNonWhitelisted: true,
+      transform: true,
+      whitelist: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
