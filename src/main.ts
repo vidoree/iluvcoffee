24746a7b91a55cf9fee12de9ada40c05ac6103e4
Dora Sivo <sivodora@gmail.com>
2021-05-26 15:45:18 +0200

    fix(main): replace use with useGlobalPipes

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(
+  app.useGlobalPipes(
     new ValidationPipe({
       forbidNonWhitelisted: true,
       transform: true,
