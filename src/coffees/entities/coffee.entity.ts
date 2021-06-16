06813b66c8c17ca12960c414f50c5f417a8fb395
Dora Sivo <sivodora@gmail.com>
2021-06-16 15:19:12 +0200

    feat: MongoDB module

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,19 @@
-export class Coffee {
-  id: number;
+import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+export class Coffee extends Document {
+  @Prop()
   name: string;
+
+  @Prop()
   brand: string;
+
+  @Prop([String])
   flavors: string[];
+
+  @Prop({ default: 0 })
+  recommendations: number;
 }
+
+export const CoffeeSchema = SchemaFactory.createForClass(Coffee);
